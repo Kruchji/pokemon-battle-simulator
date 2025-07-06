3e1e9f678dfbb0c9c77c1d5202af397c58681f3e
Jiří Kruchina <kruchi@centrum.cz>
2025-07-06 12:59:39 +0200

    Add test project

---
+++ PokemonBattleSimulator_Tests/Pokemon_Tests.cs
@@ -0,0 +1,24 @@
+namespace PokemonBattleSimulator_Tests;
+
+using PokemonBattleSimulator;
+
+public class Pokemon_Tests
+{
+    [Fact]
+    public void CreatePikachu_Success()
+    {
+        var pikachu = new Pokemon("Pikachu", 10, 20, 30, 40, 50, 60, 70, new Move("Thunderbolt", 90, 100, 15, PokemonType.Electric, MoveCategory.Special), PokemonType.Electric);
+
+        Assert.NotNull(pikachu);
+        Assert.Equal("Pikachu", pikachu.Name);
+        Assert.Equal(10, pikachu.Level);
+        Assert.Equal(20, pikachu.Health);
+        Assert.Equal(30, pikachu.Attack);
+        Assert.Equal(40, pikachu.Defense);
+        Assert.Equal(50, pikachu.Speed);
+        Assert.Equal(60, pikachu.SpecialAttack);
+        Assert.Equal(70, pikachu.SpecialDefense);
+        Assert.Equal(PokemonType.Electric, pikachu.FirstType);
+        Assert.Null(pikachu.SecondType);
+    }
+}
